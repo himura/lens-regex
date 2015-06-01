ee5f0ec9c948057a33c617f41ad436319df56dfe
Takahiro HIMURA <taka@himura.jp>
2015-06-02 02:54:30 +0900

    Add `matched` optic

--- src/Text/Regex/Lens.hs
+++ src/Text/Regex/Lens.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Text.Regex.Lens
        ( regex
+       , regex'
        , matched
+       , matched'
        ) where
 
 import Control.Lens
@@ -14,19 +17,32 @@ import qualified Data.Array as A
 type RegexResult = [RegexPartialResult]
 
 data RegexPartialResult
-    = RegexMatchedPart String MatchArray
+    = RegexMatchedPart (String, MatchArray)
     | RegexOtherPart String
     deriving Show
 
-regex :: String -> Lens' String RegexResult
-regex pat f target = fromRegexResult <$> f (toRegexResult pat target)
+regex :: (Indexable Int p, Applicative f)
+      => String
+      -> p (String, MatchArray) (f (String, MatchArray))
+      -> String -> f String
+regex pat = regex' pat . matched
 
-matched :: Traversal' [RegexPartialResult] String
-matched afb s = go s
+regex' :: String -> Lens' String RegexResult
+regex' pat f target = fromRegexResult <$> f (toRegexResult pat target)
+
+matched :: (Choice p, Indexable Int p, Applicative f, Traversable t)
+        => p (String, MatchArray) (f (String, MatchArray))
+        -> t RegexPartialResult -> f (t RegexPartialResult)
+matched = traversed . matched'
+
+matched' :: (Choice p, Applicative f) => Optic' p f RegexPartialResult (String, MatchArray)
+matched' = dimap matchedStr toRes . right'
   where
-    go ((RegexMatchedPart str marr):xs) = (:) <$> (RegexMatchedPart <$> afb str <*> pure marr) <*> go xs
-    go (x:xs) = (:) <$> pure x <*> go xs
-    go [] = pure []
+    matchedStr (RegexMatchedPart s) = Right s
+    matchedStr (RegexOtherPart s) = Left s
+
+    toRes (Left s) = pure (RegexOtherPart s)
+    toRes (Right x) = RegexMatchedPart <$> x
 
 toRegexResult :: String -> String -> RegexResult
 toRegexResult pat target = go 0 matchList
@@ -41,10 +57,10 @@ toRegexResult pat target = go 0 matchList
       where
         pl@(pos', len) = m A.! 0
         posDiff = pos' - pos
-        cont = RegexMatchedPart (extract pl target) m : go (pos' + len) ms
+        cont = RegexMatchedPart ((extract pl target), m) : go (pos' + len) ms
 
 fromRegexResult :: RegexResult -> String
 fromRegexResult = concat . map toStr
   where
-    toStr (RegexMatchedPart s _) = s
+    toStr (RegexMatchedPart (s, _)) = s
     toStr (RegexOtherPart s) = s
