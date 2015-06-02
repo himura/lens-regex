d5e803691d886d36629e1b98821690e2348814f8
Takahiro HIMURA <taka@himura.jp>
2015-06-03 03:15:04 +0900

    Add regexTraverse

--- src/Text/Regex/Lens.hs
+++ src/Text/Regex/Lens.hs
@@ -3,7 +3,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Text.Regex.Lens
-       ( regex
+       ( MatchPart(..)
+       , matchedString
+       , backreferences
+       , iregexTraverse
+       , regexTraverse
+
+       , regex
        , regex'
        , filterMatched
        ) where
@@ -31,6 +37,18 @@ regex pat = regex' pat . traversed . filterMatched
 
 regex' :: String -> Lens' String RegexResult
 regex' pat f target = fromRegexResult <$> f (toRegexResult pat target)
+
+regexTraverse :: String -> Traversal' String MatchPart
+regexTraverse pat f target = fromRegexResult <$> go (toRegexResult pat target)
+  where
+    go [] = pure []
+    go ((Left x):xs) = ((Left x):) <$> go xs
+    go ((Right x):xs) = (:) <$> (Right <$> f x) <*> go xs
+
+iregexTraverse :: (Indexable Int p, Applicative f)
+               => String
+               -> p MatchPart (f MatchPart) -> String -> f String
+iregexTraverse pat = conjoined (regexTraverse pat) (indexing (regexTraverse pat))
 
 filterMatched :: (Choice p, Applicative f) => Optic' p f RegexPartialResult MatchPart
 filterMatched = dimap id (either (pure . Left) (Right <$>)) . right'
