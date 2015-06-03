ed949059acb1482bba1cce9db027c51edfe443d4
Takahiro HIMURA <taka@himura.jp>
2015-06-04 03:36:23 +0900

    Add output example

--- sample/sample.hs
+++ sample/sample.hs
@@ -1,5 +1,42 @@
 {-# LANGUAGE QuasiQuotes #-}
 
+{-
+
+OUTPUT:
+
+##################
+## Target String:
+target1 = "hoge00 fuga hoge01 neko hoge02"
+
+## Example: target1 ^.. regex [r|hoge[0-9]+|]
+[MatchPart {_matchedString = "hoge00", _captures = []},MatchPart {_matchedString = "hoge01", _captures = []},MatchPart {_matchedString = "hoge02", _captures = []}]
+
+## Example: target1 ^.. regex [r|hoge[0-9]+|] . matchedString
+["hoge00","hoge01","hoge02"]
+
+## Example: target1 ^? regex [r|hoge[0-9]+|] . index 1 . matchedString
+Just "hoge01"
+
+## Example: target1 ^? regex [r|hoge[0-9]+|] . index 3 . matchedString
+Nothing
+
+## Example: target1 & regex [r|hoge[0-9]+|] . matchedString .~ "HOGE"
+"HOGE fuga HOGE neko HOGE"
+
+## Example: target1 & regex [r|hoge[0-9]+|] .index 1 . matchedString .~ "HOGE"
+"hoge00 fuga HOGE neko hoge02"
+
+## Example: target1 & regex [r|hoge[0-9]+|] .index 1 . matchedString %~ (\s -> "<<" ++ s ++ ">>")
+"hoge00 fuga <<hoge01>> neko hoge02"
+##################
+## Target String:
+target2 = "<img src=\"/image/shinku0721.jpg\" alt=\"shinku birthday\"><img src=\"/image/shinku141.jpg\">"
+
+## Example: target2 ^.. regex [r|<img src="([^"]+)"[^>]*>|] . captures . traversed . index 0
+["/image/shinku0721.jpg","/image/shinku141.jpg"]
+
+-}
+
 module Main where
 
 import Control.Lens
@@ -42,5 +79,3 @@ main = do
 
     putStrLn "\n## Example: target2 ^.. regex [r|<img src=\"([^\"]+)\"[^>]*>|] . captures . traversed . index 0"
     print $ target2 ^.. regex ([r|<img src="([^"]+)"[^>]*>|] :: Regex) . captures . traversed . index 0
-
-
