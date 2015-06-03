738945d05a53a86a5a5ba1ab29a8b35cf66ea682
Takahiro HIMURA <taka@himura.jp>
2015-06-04 02:09:34 +0900

    support other string like types

--- src/Text/Regex/Lens.hs
+++ src/Text/Regex/Lens.hs
@@ -14,51 +14,48 @@ module Text.Regex.Lens
        , filterMatched
        ) where
 
-import Control.Lens
 import Control.Applicative
-import Text.Regex.Base
-import Text.Regex.Posix
+import Control.Lens
 import qualified Data.Array as A
+import Data.Monoid
+import Text.Regex.Base
 
-type RegexResult = [RegexPartialResult]
-type RegexPartialResult = Either String MatchPart
+type RegexResult text = [RegexPartialResult text]
+type RegexPartialResult text = Either text (MatchPart text)
 
-data MatchPart = MatchPart
-    { _matchedString :: String
-    , _backreferences :: [String]
+data MatchPart text = MatchPart
+    { _matchedString :: text
+    , _backreferences :: [text]
     } deriving Show
 makeLenses ''MatchPart
 
-regex :: (Indexable Int p, Applicative f)
-      => String
-      -> p MatchPart (f MatchPart)
-      -> String -> f String
+regex :: (Indexable Int p, Applicative f, RegexLike regex text, Monoid text)
+      => regex
+      -> p (MatchPart text) (f (MatchPart text))
+      -> text -> f text
 regex pat = regex' pat . traversed . filterMatched
 
-regex' :: String -> Lens' String RegexResult
+regex' :: (RegexLike regex text, Monoid text) => regex -> Lens' text (RegexResult text)
 regex' pat f target = fromRegexResult <$> f (toRegexResult pat target)
 
-regexTraverse :: String -> Traversal' String MatchPart
+regexTraverse :: (RegexLike regex text, Monoid text) => regex -> Traversal' text (MatchPart text)
 regexTraverse pat f target = fromRegexResult <$> go (toRegexResult pat target)
   where
     go [] = pure []
     go ((Left x):xs) = ((Left x):) <$> go xs
     go ((Right x):xs) = (:) <$> (Right <$> f x) <*> go xs
 
-iregexTraverse :: (Indexable Int p, Applicative f)
-               => String
-               -> p MatchPart (f MatchPart) -> String -> f String
+iregexTraverse :: (Indexable Int p, Applicative f, RegexLike regex text, Monoid text)
+               => regex
+               -> p (MatchPart text) (f (MatchPart text)) -> text -> f text
 iregexTraverse pat = conjoined (regexTraverse pat) (indexing (regexTraverse pat))
 
-filterMatched :: (Choice p, Applicative f) => Optic' p f RegexPartialResult MatchPart
+filterMatched :: (Choice p, Applicative f) => Optic' p f (RegexPartialResult text) (MatchPart text)
 filterMatched = dimap id (either (pure . Left) (Right <$>)) . right'
 
-toRegexResult :: String -> String -> RegexResult
-toRegexResult pat target = go 0 matchList
+toRegexResult :: RegexLike regex text => regex -> text -> (RegexResult text)
+toRegexResult pat target = go 0 $ matchAll pat target
   where
-    matchList :: [A.Array Int (MatchOffset, MatchLength)]
-    matchList = target =~ pat
-
     go pos [] = [Left (after pos target)]
     go pos (m:ms) =
         if posDiff > 0
@@ -70,8 +67,8 @@ toRegexResult pat target = go 0 matchList
         (ms0:mss) = map (flip extract target) $ A.elems m
         cont = Right (MatchPart ms0 mss) : go (pos' + len) ms
 
-fromRegexResult :: RegexResult -> String
-fromRegexResult = concat . map toStr
+fromRegexResult :: Monoid text => (RegexResult text) -> text
+fromRegexResult = mconcat . map toStr
   where
     toStr (Right (MatchPart s _)) = s
     toStr (Left s) = s
